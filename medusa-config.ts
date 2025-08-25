156ed9ccd8649930428455daad29d9b2f6badc31
amartfpro <amartfpro@gmail.com>
2025-08-25 20:31:36 +0200

    More fixes for deployment

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,9 +5,11 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    workerMode: (["shared", "worker", "server"].includes(process.env.MEDUSA_WORKER_MODE as string)
-      ? (process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server")
-      : "shared"),
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    admin: {
+      disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    },
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
